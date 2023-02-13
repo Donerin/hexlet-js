5977dbb07fe78992c23182d2b4de26c0bdb4bbc4
donerin <donerin1404@gmail.com>
2023-02-13 16:38:29 +0300

    del and add changes

--- src/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import { strict as assert } from 'node:assert';
-import { capitalize } from '../src/capitalize.js';
-
-assert(capitalize('') === '');
-assert(capitalize('hello') === 'Hello');
-
-console.log('Все тесты пройдены!');
